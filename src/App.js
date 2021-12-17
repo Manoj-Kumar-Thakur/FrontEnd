0397dee382d8adbbb3ed5e72f907ed2cb17b783e
Thakur <manoj-kumar.thakur@capgemini.com>
2021-12-17 22:20:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import store from './store/store'
+import HomeRouter from './routers/HomeRouter';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div>
+            <Provider store={store()}>
+                <HomeRouter />
+            </Provider>
+        </div>
+
+    );
+
+
 }
 
 export default App;
