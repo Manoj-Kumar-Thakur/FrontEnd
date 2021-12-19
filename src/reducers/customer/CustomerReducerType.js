83a3481a999b72cea9b2cdc923b1b45f2b91bf61
Thakur <manoj-kumar.thakur@capgemini.com>
2021-12-19 23:19:38 +0530

    updated

--- src/reducers/customer/CustomerReducerType.js
+++ src/reducers/customer/CustomerReducerType.js
@@ -1,32 +1,31 @@
 import { ActionType } from "../../actions/customer/ActionType";
 
-const initialState ={
-    customers:[]
+const initialState = {
+  customers: [],
 };
 
-export const getCustomersReducer = (state = initialState,{type,payload}) => {
-    switch (type) {
-        case ActionType.GET_CUSTOMERS:
-            return {...state,customers:payload}
-            
-    
-        default:
-            return state;
-            
-    }
+export const getCustomersReducer = (
+  state = initialState,
+  { type, payload }
+) => {
+  switch (type) {
+    case ActionType.GET_CUSTOMERS:
+      return { ...state, customers: payload };
+
+    default:
+      return state;
+  }
 };
 
-export const getCustomerReducer = (state = {} ,{type,payload}) => {
-    switch (type) {
-        case ActionType.GET_CUSTOMER_ID:
-            return {...state,...payload}
-            
+export const getCustomerReducer = (state = {}, { type, payload }) => {
+  switch (type) {
+    case ActionType.GET_CUSTOMER_ID:
+      return { ...state, ...payload };
 
-        case ActionType.REMOVE_GET_CUSTOMER_ID:
-            return{}
-            
-    
-        default:
-            return state;
-    }
-}
+    case ActionType.REMOVE_GET_CUSTOMER_ID:
+      return {};
+
+    default:
+      return state;
+  }
+};
--- src/routers/customer/CustomerRouter.js
+++ src/routers/customer/CustomerRouter.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import AddCustomer from '../../components/customer/AddCustomer';
-import DashBoard from '../../components/customer/DashBoard';
-import UpdateCustomer from '../../components/customer/UpdateCustomer';
-import Customer from '../../components/customer/Customer';
-
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import AddCustomer from "../../components/customer/AddCustomer";
+import DashBoard from "../../components/customer/DashBoard";
+import UpdateCustomer from "../../components/customer/UpdateCustomer";
+import Customer from "../../components/customer/Customer";
 
 const CustomerRouter = () => (
-    <BrowserRouter>
-         <div>
-             <Switch>
-                 <Route path={`/customer`} component={DashBoard}/>
-                 <Route path={`/addCustomer`} component={AddCustomer}/>
-                 <Route path={`/updateCustomer`} component={UpdateCustomer} />
-                 <Route path={`/getCustomer/:userId`} component={Customer} />   
-
-             </Switch>
-         </div>
-    </BrowserRouter>
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route path={`/customer`} component={DashBoard} />
+        <Route path={`/addCustomer`} component={AddCustomer} />
+        <Route path={`/updateCustomer`} component={UpdateCustomer} />
+        <Route path={`/getCustomer/:userId`} component={Customer} />
+      </Switch>
+    </div>
+  </BrowserRouter>
 );
 
 export default CustomerRouter;
